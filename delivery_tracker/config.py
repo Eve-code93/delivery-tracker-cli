074f1e841debb464a5f7a79a4971e7c91b4790ad
Eve-code93 <evelynejose1993@gmail.com>
2025-05-26 11:31:02 +0300

    created a virtual environment and set up sqlalchemy and alembic

---
+++ delivery_tracker/config.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///delivery_tracker.db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
